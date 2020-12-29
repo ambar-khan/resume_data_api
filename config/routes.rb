b31fcb99ac37b4d7cf25e397e69c09ce8f6c022d
Ambar <ambarmogg@me.com>
2020-12-28 18:47:21 -0800

    migrations to change user_id to student_id

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
   # get "/photos" => "photos#index"
 
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
+   # EXAMPLE JSON ROUTE WITH API NAMESPACE
+  
+  
+  
+  
   # namespace :api do
-  #   get "/photos" => "photos#index"
+  #   get "/experience" => "experience#index"
   # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_29_003835) do
+ActiveRecord::Schema.define(version: 2020_12_29_024128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2020_12_29_003835) do
     t.string "degree"
     t.string "university_name"
     t.text "details"
-    t.integer "user_id"
+    t.integer "student_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2020_12_29_003835) do
     t.string "job_title"
     t.string "company_name"
     t.text "details"
-    t.integer "user_id"
+    t.integer "student_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
