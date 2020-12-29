39689f0146d427f64f6cde61aca063095e43072d
Andrew Chin <andrewschin9@gmail.com>
2020-12-28 21:18:38 -0600

    remove duplicate students in schema

--- db/schema.rb
+++ db/schema.rb
@@ -68,22 +68,4 @@ ActiveRecord::Schema.define(version: 2020_12_29_020408) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
-
-  create_table "students", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "phone_number"
-    t.text "short_bio"
-    t.string "linkedin_url"
-    t.string "twitter_handle"
-    t.string "personal_blog_url"
-    t.string "resume_url"
-    t.string "github_url"
-    t.string "photo"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
 end
