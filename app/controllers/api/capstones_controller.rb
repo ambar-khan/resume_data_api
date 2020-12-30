5acf4950274d1998f699336942e849684b7a503c
Alex Racinski <aracinski2@gmail.com>
2020-12-29 18:48:12 -0600

    add routes to capstones, fix create action

--- app/controllers/api/capstones_controller.rb
+++ app/controllers/api/capstones_controller.rb
@@ -17,6 +17,7 @@ class Api::CapstonesController < ApplicationController
       student_id: params[:student_id]
     )
     @capstone.save
+    render 'show.json.jb'
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
 
     get "/capstones" => "capstones#index"
     get "/capstones/:id" => "capstones#show"
+    post "/capstones" => "capstones#create"
+    patch "/capstones/:id" => "capstones#update"
+    delete "/capstones/:id" => "capstones#destroy"
 
 
     get "/experiences" => "experiences#index"
