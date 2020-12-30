213f5908de72cf5940239b662c068e8b191a0967
Andrew Chin <andrewschin9@gmail.com>
2020-12-29 18:39:46 -0600

    add destroy to students, experience, and education controller plus clean up syntax

--- config/routes.rb
+++ config/routes.rb
@@ -2,26 +2,29 @@ Rails.application.routes.draw do
   namespace :api do
     get "/students" => "students#index"
     post "/students" => "students#create"
-    
+    get "students/:id" => "students#show"
+    patch "/students/:id" => "students#update"
+    delete "/students/:id" => "students#destroy"    
+
+
     get "/skills" => "skills#index"
+    post "/skills" => "skills#create"
+    get "/skills/:id" => "skills#show"
+    patch "/skills/:id" => "skills#update"
+    delete "/skills/:id" => "skills#destroy"
+
 
     get "/experiences" => "experiences#index"
     get "/experiences/:id" => "experiences#show"
     post "/experiences" => "experiences#create"
     patch "/experiences/:id" => "experiences#update"
+    delete "/experiences/:id" => "experiences#destroy"
 
     get "/educations" => "educations#index"
     get "/educations/:id" => "educations#show"
     post "/educations" => "educations#create"
-    patch "/educations" => "educations#update"
+    patch "/educations/:id" => "educations#update"
+    delete "/educations/:id" => "educations#destroy"
 
-  end
-
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  namespace :api do
-    get "/students" => "students#index"
-    post "/students" => "students#create"
-    get "/skills" => "skills#index"
-    get "students/:id" => "students#show"
   end
 end
